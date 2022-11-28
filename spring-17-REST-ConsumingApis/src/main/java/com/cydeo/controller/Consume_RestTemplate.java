d2d1928a213cf59acbe2bcffccea13a3b0903483
naranburulov <naranburulov@gmail.com>
2022-11-28 13:06:02 -0500

    Rest Template controller cases are finished and tested

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,12 +1,11 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.User;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/cydeo")
@@ -21,9 +20,33 @@ public class Consume_RestTemplate {
 
 
     @GetMapping
-    public ResponseEntity<User[]> readAllUsers(){       //accepting array
+    public ResponseEntity<User[]> readAllUsers(){       //returning DTO (accepting array)
 
-        return restTemplate.getForEntity(URI, User[].class);
+        return restTemplate.getForEntity(URI, User[].class);    //getForEntity() gets url, and shows api objects
+    }
+
+    @GetMapping("/{id}")                                            //another way of retrieving from api by returning Object
+    public Object readUser(@PathVariable("id") Integer id){
+
+        String URL = URI + "/{id}";
+
+        return restTemplate.getForObject(URL, Object.class, id);
+    }
+
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostFromDummyApi(){
+
+//in case, if api has security and is requesting app id: "error": "APP_ID_MISSING"
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id", "6298ebfecd0551211fce37a6");                  //app id
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange("https://dummyapi.io/data/v1/user?limit=10", HttpMethod.GET,entity,Object.class);
+
     }
 
 
