1ffa3cb55b9af7ee61cf11a88d59d3fb182e06c3
naranburulov <naranburulov@gmail.com>
2022-11-29 11:56:59 -0500

    module 15 - Jakson annotations are learnt;
    Lab - module18 started with DTO's annotations

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -1,12 +1,19 @@
 package com.cydeo.dto;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+// similar to @JsonIgnore, but at the class level, not the field
+//@JsonIgnoreProperties(value = {"address", "country"}, ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true) //will ignore unknown fields - good practice, to save running time
 public class AccountDTO {
 
+    @JsonIgnore
     private String name;
     private String address;
     private String country;
@@ -14,6 +21,8 @@ public class AccountDTO {
     private String city;
     private Integer age;
     private String postalCode;
+
+    @JsonBackReference          //hides UserDTO (this field is not going to be serialized)
     private UserDTO user;
 
 }
--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
@@ -9,9 +12,14 @@ import lombok.*;
 public class UserDTO {
 
     private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)       //property means a field;
+    // here it means user can read, but not write
     private String password;
     private String username;
     private UserRole role;
+
+    @JsonManagedReference               //show AccDTO in UserDTO (this field is going to be serialized)
     private AccountDTO account;
 
 }
