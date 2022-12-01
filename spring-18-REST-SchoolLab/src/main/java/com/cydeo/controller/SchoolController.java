e86ff91b3f40dfbcb7850284a409a8c43e8de07c
naranburulov <naranburulov@gmail.com>
2022-12-01 11:07:06 -0500

    Module 19 started

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,11 +1,16 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.AddressDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.AddressService;
+import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,10 +20,14 @@ public class SchoolController {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
+    private final AddressService addressService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
+        this.addressService = addressService;
     }
 
 
@@ -31,8 +40,25 @@ public class SchoolController {
     @GetMapping("/students")
     public ResponseEntity<ResponseWrapper> readAllStudents(){
         return ResponseEntity.ok(new ResponseWrapper("Students are successfully retrieved", studentService.findAll()));
-
+        //if we use ResponseEntity.ok, then we put the data(body) directly as above
     }
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+        ResponseWrapper responseWrapper = new ResponseWrapper(true, "Parents are retrieved succesfully",
+                HttpStatus.OK.value(), parentService.findAll());
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseWrapper);
+        //if we use ResponseEntity.status(HttpStatus.OK), then we need to use .body as above
+    }
+
+    @GetMapping("/address/{id}")
+    public ResponseEntity<ResponseWrapper> getAddress(@PathVariable("id") Long id) throws Exception {
+        AddressDTO addressDTO = addressService.findById(id);
+
+        return ResponseEntity.ok(new ResponseWrapper("Address is successfully retrieved", addressDTO));
+    }
+
 
 
 
