68fe1092bd9d1f1823e70b49a31dead40be989ae
naranburulov <naranburulov@gmail.com>
2022-12-02 14:15:48 -0500

    Modeule18 - WeatherDTO added

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -9,9 +9,7 @@ import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,6 +56,16 @@ public class SchoolController {
 
         return ResponseEntity.ok(new ResponseWrapper("Address is successfully retrieved", addressDTO));
     }
+
+    @PutMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id") long id, @RequestBody AddressDTO addressDTO) throws Exception {
+        addressDTO.setId(id);
+        return addressService.update(addressDTO);
+
+    }
+
+
+
 
 
 
