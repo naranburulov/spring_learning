bf88c5e4a97ef1a0217aec05fe35239d161ad7d4
naranburulov <naranburulov@gmail.com>
2022-12-16 21:21:54 -0500

    TaskServiceImplTest tested -  @ParameterizedTest

--- spring-23-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/TaskServiceImplTest.java
+++ spring-23-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/TaskServiceImplTest.java
@@ -5,6 +5,7 @@ import com.cydeo.entity.Task;
 import com.cydeo.mapper.TaskMapper;
 import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -15,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,15 +36,38 @@ class TaskServiceImplTest {
     @ValueSource(longs = {1L, 2L, 3L})
     void findById_Test(long id){
 
+        //GIVEN (preparation):
         Task task = new Task();
 
         when(taskRepository.findById(id)).thenReturn(Optional.of(task));    //returns Optional<Task>
         when(taskMapper.convertToDto(task)).thenReturn(new TaskDTO());
 
+        //WHEN (action is happening):
         taskService.findById(id);
 
+        //THEN (assertion and verification checks/TestResults):
         verify(taskRepository).findById(id);
         verify(taskMapper).convertToDto(task);
+
+    }
+
+    @Test
+    void findBy_BDD_Test(){
+
+        //Given
+        Task task = new Task();
+        given(taskRepository.findById(anyLong())).willReturn(Optional.of(task));
+        given(taskMapper.convertToDto(task)).willReturn(new TaskDTO());
+
+        //When
+        taskService.findById(anyLong());
+
+        //Then
+        then(taskRepository).should().findById(anyLong());
+        then(taskMapper).should(atLeastOnce()).convertToDto(task);
+
+
+
 
     }
 
