3923f47cdcc06033c4c801436fa294e62aed1154
naranburulov <naranburulov@gmail.com>
2022-12-16 21:09:42 -0500

    TaskServiceImplTest tested

---
+++ spring-23-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/TaskServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
+import com.cydeo.service.TaskService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceImplTest {
+
+    @Mock
+    TaskRepository taskRepository;
+
+    @Mock
+    TaskMapper taskMapper;
+
+    @InjectMocks
+    TaskServiceImpl taskService;
+
+    @ParameterizedTest
+    @ValueSource(longs = {1L, 2L, 3L})
+    void findById_Test(long id){
+
+        Task task = new Task();
+
+        when(taskRepository.findById(id)).thenReturn(Optional.of(task));    //returns Optional<Task>
+        when(taskMapper.convertToDto(task)).thenReturn(new TaskDTO());
+
+        taskService.findById(id);
+
+        verify(taskRepository).findById(id);
+        verify(taskMapper).convertToDto(task);
+
+    }
+
+
+
+}
