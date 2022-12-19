f75244dd447a9d5d0086abc6f8030a2130d3a08c
naranburulov <naranburulov@gmail.com>
2022-12-18 21:17:28 -0500

    WelcomeControllerTest is finished

--- spring-24-UnitTesting-MockMVC/src/test/java/com/cydeo/controller/Spring24UnitTestingMockMvcApplicationTests.java
+++ spring-24-UnitTesting-MockMVC/src/test/java/com/cydeo/controller/Spring24UnitTestingMockMvcApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cydeo;
+package com.cydeo.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ spring-24-UnitTesting-MockMVC/src/test/java/com/cydeo/controller/StudentControllerTest.java
@@ -0,0 +1,7 @@
+package com.cydeo.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(StudentController.class)
+public class StudentControllerTest {
+}
